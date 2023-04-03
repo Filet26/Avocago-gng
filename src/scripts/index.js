c4806ad6e82e348c8346246c2b280cd0626be9de
Desmond Ho <kakacake666@gmail.com>
2023-04-02 22:16:35 -0700

    MUSIC

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,19 @@ const body = document.body;
 discoBtn.addEventListener("click", () => {
   body.classList.toggle("disco-mode");
 });
+
+//  MUSIC
+
+const button = document.getElementById("disco-btn");
+const audio = document.getElementById("hamter");
+let isPlaying = false;
+
+button.addEventListener("click", () => {
+  if (!isPlaying) {
+    audio.play();
+    isPlaying = true;
+  } else {
+    audio.pause();
+    isPlaying = false;
+  }
+});
